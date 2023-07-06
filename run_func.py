8e13a6f0c056ee25df0a5759e3b28175eb766bfa
zy-Liang <zyliang@umich.edu>
2023-07-06 16:05:54 -0400

    Update run_func to submit job

--- run_func.py
+++ run_func.py
@@ -1,20 +1,38 @@
 from globus_compute_sdk import Executor
 from pprint import pprint
-import time
 
 
 # First, define the function ...
-def add_func(a, b):
+def submit_job():
+    import subprocess
     import time
-    time.sleep(10)
-    return a + b
+    # submit a new job to slurm
+    job_script = "/home/zyliang/ondemand/data/sys/myjobs/projects/default/9/main_job.sh"
+    account = "zyliang"
+    output = subprocess.check_output(["sbatch", job_script]).decode()
+    output = output.strip()
+    # get the job id of the submitted job
+    job_id = [ int(i) for i in output.split() if i.isdigit() ]
+    job_id = job_id[0]
+    time_interval = 5
+    # iteratively check if the submitted job is completed
+    while True:
+        time.sleep(time_interval)
+        output = subprocess.check_output(["squeue", "-u", account]).decode()
+        output = output.strip()
+        if not str(job_id) in output:
+            break
+    # read and print the output of slurm
+    with open(f"slurm-{job_id}.out", "r") as output_file:
+        output = output_file.read()
+        return output
 
-tutorial_endpoint_id = '6b4b7c27-3fd5-4f12-a529-96f2f1dfffdf' # Public tutorial endpoint
-#tutorial_endpoint_id = '657ee312-da8a-497e-9026-a6faf4b16cf8' # Public tutorial endpoint
+
+test_endpoint_id = '53d7e6d2-2e37-4991-91c5-e8a37009e811'
 # ... then create the executor, ...
-with Executor(endpoint_id=tutorial_endpoint_id) as gce:
+with Executor(endpoint_id=test_endpoint_id) as gce:
     # ... then submit for execution, ...
-    future = gce.submit(add_func, 5, 10)
-
+    future = gce.submit(submit_job)
+    print("\nSubmitted the function to Globus endpoint.\n")
     # ... and finally, wait for the result
     print(future.result())
