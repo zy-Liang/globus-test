f416c8aef881d5ee3dfa39f02494e36763f994ac
zy-Liang <zyliang@umich.edu>
2023-07-15 14:32:42 -0400

    Support single prompt in

--- run_func.py
+++ run_func.py
@@ -3,11 +3,11 @@ from pprint import pprint
 
 
 # First, define the function ...
-def submit_job():
+def submit_job(prompt:str = "The capital of France is"):
     import subprocess
     # run llama with torch
     output = subprocess.run(["torchrun", "--nproc_per_node", "1", 
-                             "/home/zyliang/llama-test/llama/example.py",
+                             "/home/zyliang/llama-test/llama/run.py", "--prompt", prompt,
                              "--ckpt_dir", "/nfs/turbo/umms-dinov/LLaMA/1.0.1/llama/modeltoken/7B",
                              "--tokenizer_path", "/nfs/turbo/umms-dinov/LLaMA/1.0.1/llama/modeltoken/tokenizer.model"],
                             capture_output=True)
@@ -21,7 +21,8 @@ test_endpoint_id = 'b9d9099c-4aed-499c-a020-743041a15521'
 # ... then create the executor, ...
 with Executor(endpoint_id=test_endpoint_id) as gce:
     # ... then submit for execution, ...
-    future = gce.submit(submit_job)
+    prompt = "Hello LLaMA!"
+    future = gce.submit(submit_job, prompt)
     print("\nSubmitted the function to Globus endpoint.\n")
     # ... and finally, wait for the result
     # print(future.result())
